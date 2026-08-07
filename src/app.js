60e930bccb1aaa1d662e8cfaaac24079e52141c4
DC117 <pdeepti2311@gmail.com>
2026-08-07 16:05:57 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+// app.use("/", (req, res) => {
+//   res.send("Hello from the  Deepti.....!!!!");
+// });
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello Hello!!!");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from the server !!!");
+});
+app.listen(7777, () => {
+  console.log("Server is successfully running");
+});
