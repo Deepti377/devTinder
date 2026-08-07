12e284f90f615daad44fb6613b1873588b4ae907
DC117 <pdeepti2311@gmail.com>
2026-08-07 22:45:29 +0530

    exploring get put and delerte api

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 
 const app = express();
-// app.use("/", (req, res) => {
-//   res.send("Hello from the  Deepti.....!!!!");
-// });
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello!!!");
+
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Deepti",
+    lastName: "Chauhan",
+  });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save Data to database");
+  res.send("Data Successfully saved to database");
+});
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully ");
 });
 app.use("/test", (req, res) => {
   res.send("Hello from the server !!!");
 });
+
 app.listen(7777, () => {
   console.log("Server is successfully running");
 });
