8805a43477d5e273ec8be6327b07a1893c14e932
DC117 <pdeepti2311@gmail.com>
2026-08-18 21:50:19 +0530

    L-7 Update delete api and documentation

--- src/app.js
+++ src/app.js
@@ -3,19 +3,88 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user.js");
 
+// json parser
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Nidhi",
-    lastName: "Nidhi",
-    emailId: "nidhi@123.com",
-    password: "nidhi@123",
-  });
+  console.log(req.body);
+  const user = new User(req.body);
+
   try {
     // Data will be saved to our database using save method
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+// Detele a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    //const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body?.userId;
+  const data = req.body;
+
+  try {
+    // const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    // const isUpdateAllowed = Object.keys(data).every((k) =>
+    //   ALLOWED_UPDATES.includes(k)
+    // );
+    // if (!isUpdateAllowed) {
+    //   throw new Error("Update not allowed");
+    // }
+    // if (data?.skills.length > 10) {
+    //   throw new Error("Skills cannot be more than 10");
+    // }
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:");
   }
 });
 
@@ -26,11 +95,6 @@ connectDB()
   .catch(() => {
     console.error("Database connection established...");
   });
-
-//   Creating a new instance of the User model
-// const user = new User(req.body);
-
-// });
 
 app.listen(7777, () => {
   console.log("Server is successfully running");
