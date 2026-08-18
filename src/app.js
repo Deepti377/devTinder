31ed33394a30afbd17dc1a1c3e4194ce6aa8fe6c
DC117 <pdeepti2311@gmail.com>
2026-08-18 15:59:43 +0530

    moongoose database L-6

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 require("./config/database");
 const app = express();
+const User = require("./models/user.js");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Nidhi",
+    lastName: "Nidhi",
+    emailId: "nidhi@123.com",
+    password: "nidhi@123",
+  });
+
+  //   Creating a new instance of the User model
+  // const user = new User(req.body);
+
+  try {
+    // Data will be saved to our database using save method
+    await user.save();
+    res.send("User Added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 app.listen(7777, () => {
   console.log("Server is successfully running");
--- src/config/database.js
+++ src/config/database.js
@@ -1,36 +1,17 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+
 // Connection URL
-const url =
-  "mongodb+srv://pdeepti2311_db_user:oWFBcvEZI70Vound@deeptinodejs.ixfhcak.mongodb.net/";
-const client = new MongoClient(url);
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://pdeepti2311_db_user:oWFBcvEZI70Vound@deeptinodejs.ixfhcak.mongodb.net/deeptiNodeJs"
+  );
+};
 
-// Database Name
-const dbName = "HelloWorld";
-
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log("Connected successfully to server and database");
-  const db = client.db(dbName);
-  const collection = db.collection("User");
-
-  const data = {
-    firstName: "Deepika",
-    lastname: "Padukons",
-    city: "Mumbai",
-  };
-
-  const insertResult = await collection.insertMany([data]);
-  console.log("Inserted documents =>", insertResult);
-
-  //Read
-  const findResult = await collection.find({}).toArray();
-  console.log("Found documents =>", findResult);
-
-  return "done.";
-}
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+// connectDB()
+//   .then(() => {
+//     console.log("Database connection established...");
+//   })
+//   .catch(() => {
+//     console.error("Database connection established...");
+//   });
+module.exports = connectDB;
